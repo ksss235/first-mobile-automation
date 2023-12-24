25293e5fab66531b8b9db0af1c120c579ae78e36
KasumovEmel <kasumov.emel@gmail.com>
2023-12-24 14:27:06 +0300

    My first commit (#2)
    Co-authored-by: emel93 <kasumov.emel@netcracker.com>

--- src/main/java/kz/bcc/Main.java
+++ src/main/java/kz/bcc/Main.java
@@ -2,6 +2,6 @@ package kz.bcc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        }
+        System.out.println("Hello Kisusha!!!!");
     }
+}

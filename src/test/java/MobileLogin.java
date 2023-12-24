6f95adadaa32a6222d27f75de636209ed010bd38
ksss235 <bugrick23582@gmail.com>
2023-12-24 16:41:59 +0600

    added sout

--- src/test/java/MobileLogin.java
+++ src/test/java/MobileLogin.java
@@ -35,7 +35,7 @@ public class MobileLogin {
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("kz.bcc.starbanking.stage:id/fragment_container")));
         WebElement bills = driver.findElement(By.id("kz.bcc.starbanking.stage:id/navigation_bar_item_large_label_view"));
         String results = bills.getText();
-//        System.out.println(results);
+        System.out.println(results);
         Assert.assertEquals("Should be bills", "Счета", results);
     }
 
